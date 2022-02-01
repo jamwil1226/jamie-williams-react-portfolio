ff4ffe5f55f8bfb33160a5f257e94708b4c3741a
Jamie Williams <jamwil1226@yahoo.com>
2022-02-01 10:27:58 -0500

    dance inventory project added to portfolio

--- src/projectList.js
+++ src/projectList.js
@@ -2,13 +2,21 @@ import originalPortfolio from "./images/original-portfolio.png";
 import noteTaker from "./images/note-taker.png";
 import myPlate from "./images/myplate-project.png";
 import spaceExplorer from "./images/space-explorer.png";
-import weatherDashboard from "./images/weather-dashboard.png";
+import costumeInventory from "./images/costume-inventory.png";
 import workDayScheduler from "./images/workday-scheduler.png";
 
 // 6 projects with links to GitHub and website
 export default [
     {
         "id": 1,
+        "name": "Costume Inventory",
+        "image": costumeInventory,
+        "description": "This app allows our client who is a busy dance studio director to keep track of the dance studio's inventory by separating into categories, checking in, checking out, and deleting as needed.",
+        "github": "https://github.com/Kadargo63/excelsior-inventory",
+        "deploy": ""
+    },
+    {
+        "id": 2,
         "name": "My Plate",
         "image": myPlate,
         "description": "This app allows foodie lovers to sign in and create an account to be able to share their favorite plates of food. They can see other people's food posts and be able to get ideas of new food and restaurants to try. There are options to comment on posts, edit, and delete posts.",
@@ -16,7 +24,7 @@ export default [
         "deploy": "https://vast-ridge-96690.herokuapp.com/"
     },
     {
-        "id": 2,
+        "id": 3,
         "name": "Space Explorer",
         "image": spaceExplorer,
         "description": "This app allows space lovers to learn more about historical launches, astronauts, and view NASA's astronomy image of the day.",
@@ -24,7 +32,7 @@ export default [
         "deploy": "https://jamwil1226.github.io/space-explorer/"
     },
     {
-        "id": 3,
+        "id": 4,
         "name": "My Original Portfolio",
         "image": originalPortfolio,
         "description": "This is my original portfolio built strictly with HTML and CSS.",
@@ -32,20 +40,14 @@ export default [
         "deploy": "https://jamwil1226.github.io/Jamie-Williams-Portfolio/"
     },
     {
-        "id": 4,
+        "id": 5,
         "name": "Work Day Scheduler",
         "image": workDayScheduler,
         "description": "This app features 10 hour blocks of time and allows the user to schedule work day reminders. ",
         "github": "https://github.com/jamwil1226/work-day-scheduler",
         "deploy": "https://jamwil1226.github.io/work-day-scheduler/"
-    },{
-        "id": 5,
-        "name": "Weather Dashboard",
-        "image": weatherDashboard,
-        "description": "This app allows the user to find the weather in any city of the US including the 5 day forecast.",
-        "github": "https://github.com/jamwil1226/weather-dashboard",
-        "deploy": "https://jamwil1226.github.io/weather-dashboard/"
-    },{
+    },
+    {
         "id": 6,
         "name": "Note Taker",
         "image": noteTaker,
